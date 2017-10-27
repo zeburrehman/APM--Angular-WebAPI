1da57b2365ce58fdaf7b4500fbebd397674d8c05
Zeb ur Rehman <zrehman@ssilhr.com.pk>
2017-10-27 17:36:25 +0500

    Add project files.

---
+++ APM.Client/app/Product/productListCtrl.js
@@ -0,0 +1,61 @@
+﻿(function() {
+    "use strict";
+
+    angular.module("productManagement")
+        .controller("ProductListCtrl", productListCtrl);
+
+    function productListCtrl() {
+        var vm = this;
+
+        vm.products = [
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2009",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95
+            },
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2009",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95
+            },
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2009",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95
+            },
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2009",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95
+            },
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2009",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95
+            },
+            {
+                "productId": 1,
+                "productName": "Leaf Rake",
+                "productCode": "GDN-0011",
+                "releaseDate": "March 19, 2009",
+                "description": "Leaf rake with 48-inch wooden handle.",
+                "price": 19.95
+            }
+        ];
+    }
+}());
